ca31b2c57dcaa04e5c17b70082c833c51626d683
Valdir Borges <valdirborges.act@gmail.com>
2022-11-13 21:25:27 -0300

    completando o projeto

--- lib/dynamic_form_screen.dart
+++ lib/dynamic_form_screen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
 import 'package:flutter/material.dart';
 
 class DynamicFormScreen extends StatefulWidget {
@@ -10,11 +10,15 @@ class DynamicFormScreen extends StatefulWidget {
 
 class _DynamicFormScreenState extends State<DynamicFormScreen> {
   late int _count;
+  late String _result;
+  late List<Map<String, dynamic>> _values;
 
   @override
   void initState() {
     super.initState();
     _count = 0;
+    _result = '';
+    _values = [];
   }
 
   @override
@@ -35,6 +39,7 @@ class _DynamicFormScreenState extends State<DynamicFormScreen> {
             onPressed: () async {
               setState(() {
                 _count = 0;
+                _result = '';
               });
             }, 
             icon: Icon(Icons.refresh),
@@ -54,19 +59,58 @@ class _DynamicFormScreenState extends State<DynamicFormScreen> {
                 },
               ),
             ),
+            SizedBox(height: 10.0),
+            Text(_result),
           ],
         ),
       ),
     );
   }
 
-  _row(int index) {
+  _row(int key) {
     return Row(
       children: [
-        Text('ID: $index'),
+        Text('ID: $key'),
         SizedBox(width: 30.0),
-        Expanded(child: TextFormField()),
+        Expanded(child: TextFormField(
+          onChanged: (val) {
+            _onUpdate(key, val);
+          },
+        )),
       ],
     );
+  }
+
+  _onUpdate(int key, String val) {
+    int foundKey = -1;
+    for(var map in _values) {
+      if (map.containsKey('id')) {
+        if (map['id'] == key) {
+          foundKey = key;
+          break;
+        }
+      }
+    }
+    if (-1 != foundKey) {
+      _values.removeWhere((map) {
+        return map['id'] == foundKey;
+      });
+    }
+    Map<String, dynamic> json = {
+      'id':key, 
+      'value':{
+        "text": val,
+      }
+    };
+    _values.add(json);
+
+    setState(() {
+      _result = _prettyPrint(_values);
+    });
+  }
+
+  String _prettyPrint(jsonObject) {
+    var encoder = const JsonEncoder.withIndent('      ');
+    return encoder.convert(jsonObject);
   }
 }
